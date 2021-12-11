e1b05b1b7fbf367ba61eaa1c29a5728cd3180627
Aryan Sethi <aryan.rdps@gmail.com>
2021-12-11 19:44:29 +0530

    Completed Calculator Component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import { CalculatorBox } from "./components/CalculatorBox/CalculatorBox";
 import { Logo } from "./components/Logo/Logo";
 
 function App() {
     return (
         <div className="App">
             <Logo />
+			<CalculatorBox />
         </div>
     );
 }
---
+++ src/components/CalculatorBox/CalculatorBox.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import "./CalculatorBox.css";
+
+export const CalculatorBox = () => {
+    return (
+        <div className="wrapper">
+            <div className="app__left">
+                Left
+            </div>
+            <div className="app__right">
+                Right
+            </div>
+        </div>
+    )
+};
+
