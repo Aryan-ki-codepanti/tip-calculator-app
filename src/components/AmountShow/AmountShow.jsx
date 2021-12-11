eaee78e4eb91aeee975918d79f5d14380d96d4a1
Aryan Sethi <aryan.rdps@gmail.com>
2021-12-11 22:08:48 +0530

    Completed Result Display component and Amount Show UI

--- src/components/AmountShow/AmountShow.jsx
+++ src/components/AmountShow/AmountShow.jsx
@@ -1,17 +1,21 @@
 import React from "react";
-import dollar from "../../images/icon-dollar.svg";
+import dollar from "../../images/icon-dollar-cyan.svg";
+import "./AmountShow.css";
 
 export const AmountShow = ({ title, amount }) => {
+
+    const getNum = num => Number(num).toFixed(2);
+
     return (
-        <div class="amount-box">
+        <div className="amount-box">
             <div className="amount-left">
-                <p class="amount-title">{title}</p>
-                <span class="amount-person">/ person</span>
+                <p className="amount-title">{title}</p>
+                <p className="amount-person">/ person</p>
             </div>
             <div className="amount-right">
                 <h1 className="amount">
                     <img src={dollar} alt="" />
-                    {amount}
+                    {getNum(amount)}
                 </h1>
             </div>
         </div>
--- src/components/ResultDisplay/ResultDisplay.jsx
+++ src/components/ResultDisplay/ResultDisplay.jsx
@@ -1,14 +1,21 @@
-import React from 'react'
-import { AmountShow } from '../AmountShow/AmountShow';
+import React from "react";
+import { AmountShow } from "../AmountShow/AmountShow";
+import "./ResultDisplay.css";
 
 export const ResultDisplay = () => {
+
+    const resetActiveStyles = {
+        backgroundColor: "var(--Strong-cyan)",
+        color: "var(--Very-dark-cyan)"
+    };
+
     return (
         <div className="result-wrapper">
             <div>
-                <AmountShow title="tip amount" amount={0}/>
-                <AmountShow title="total" amount={0}/>
+                <AmountShow title="tip amount" amount={0} />
+                <AmountShow title="total" amount={0} />
             </div>
-            <button className='reset-button'>Reset</button>
+            <button className="reset-button">Reset</button>
         </div>
-    )
+    );
 };
