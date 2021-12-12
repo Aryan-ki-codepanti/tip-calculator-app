1cc6ce974224b411f8548af24bc7d13683c44874
Aryan Sethi <aryan.rdps@gmail.com>
2021-12-12 13:46:24 +0530

    Added Mobile Responsive Styles

--- src/components/TipButton/TipButton.jsx
+++ src/components/TipButton/TipButton.jsx
@@ -2,8 +2,14 @@ import React from 'react'
 import "./TipButton.css";
 
 export const TipButton = ({ percent }) => {
+
+    const buttonActiveStyles = {
+        backgroundColor: "var(--Strong-cyan)",
+        color: "var(--Very-dark-cyan)"
+    };
+
     return (
-        <button className="tip-button">
+        <button className="tip-button" >
             {percent}%
         </button>
     )
